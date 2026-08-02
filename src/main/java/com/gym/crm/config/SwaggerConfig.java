81d5758fc7aba872e122a5b0d7cc570d88a3b9db
Tokariuk Stanislav <stastokaruk1605@gmail.com>
2026-08-02 22:10:57 +0300

    replace manual config with spring boot auto-configuration

--- src/main/java/com/gym/crm/config/SwaggerConfig.java
+++ src/main/java/com/gym/crm/config/SwaggerConfig.java
@@ -1,36 +1,24 @@
 package com.gym.crm.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.gym.crm.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Gym CRM System REST API")
-                .description("REST API documentation for the Gym CRM application.")
-                .version("1.0.0")
-                .contact(new Contact("Gym Admin", "http://gym-crm.com", "admin@gym-crm.com"))
-                .build();
+    public OpenAPI gymCrmOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gym CRM System REST API")
+                        .description("REST API documentation for the Gym CRM application.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Gym Admin")
+                                .url("http://gym-crm.com")
+                                .email("admin@gym-crm.com")));
     }
 }
--- src/main/java/com/gym/crm/config/WebConfig.java
+++ src/main/java/com/gym/crm/config/WebConfig.java
@@ -6,20 +6,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.gym.crm.security.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "com.gym.crm")
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -31,24 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        jsonConverter.setObjectMapper(objectMapper());
-        converters.add(jsonConverter);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-                .resourceChain(false);
     }
 
     @Override
