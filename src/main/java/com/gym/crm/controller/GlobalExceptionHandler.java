ef6510d242bfb36c14029a1a25f501a0e5e13a5b
Tokariuk Stanislav <stastokaruk1605@gmail.com>
2026-08-03 17:39:47 +0300

    refactor redundant text

--- src/main/java/com/gym/crm/controller/GlobalExceptionHandler.java
+++ src/main/java/com/gym/crm/controller/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.gym.crm.dto.response.ErrorResponse;
 import com.gym.crm.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
